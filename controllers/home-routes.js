77319e451358123ec6e44279fd0ca3b0d07c8b0c
ktrnthsnr <asakthisner@hotmail.com>
2020-06-18 07:52:16 -0700

    added single post route and handlebar html

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -69,8 +69,7 @@ router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
       res.redirect('/');
       return;
-    }
-  
+    }  
     res.render('login');
   });
 
@@ -78,6 +77,69 @@ router.get('/login', (req, res) => {
 //     console.log(req.session);
 //     res.render('login');
 //   });
+
+// -- route to a single post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        res.render('single-post', { post });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// // -- test for single post 
+    // //test  GET post http://localhost:3001/post/1
+    // router.get('/post/:id', (req, res) => {
+    //     const post = {
+    //       id: 1,
+    //       post_url: 'https://handlebarsjs.com/guide/',
+    //       title: 'Handlebars Docs',
+    //       created_at: new Date(),
+    //       vote_count: 10,
+    //       comments: [{}, {}],
+    //       user: {
+    //         username: 'test_user'
+    //       }
+    //     };    
+    //     res.render('single-post', { post });
+    //   });
 
 module.exports = router;
 
